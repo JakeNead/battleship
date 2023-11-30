089dc74e913a5cc4cd2fee9928e9c9c5738bf377
Jake Nead <jakenead@gmail.com>
2023-11-29 22:01:26 -0800

    Delete index.test.js

--- tests/index.test.js
+++
@@ -1,9 +0,0 @@
-// const gameController = require("../src/index");
-
-// test("GameController returns a winner", () => {
-//   expect(gameController()).toEqual(expect.stringContaining("wins"));
-// });
-
-// incoming query - Assert Result
-// incoming command - Assert direct public side effects
-// outgoing command - Expect to send
