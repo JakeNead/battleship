9c4eafaaba7422b356b0a727816e1db84e518ded
Jake Nead <jakenead@gmail.com>
2023-12-04 21:53:23 -0800

    Hover on popup board displays potential ship placement

--- src/dom.js
+++ src/dom.js
@@ -1,18 +1,20 @@
-const gameController = require("./index");
+// const gameController = require("./index");
 
 function Dom() {
   function initPage() {
     const body = document.querySelector("body");
     body.innerHTML =
-      "<div id='overlay'></div><div id='popup'><h2>Place your ships</h2></div><header><h1>BATTLESHIP</h1></header><main><div id='boards'><div class='p1Board'></div><div class='p2Board'></div></div></main><footer>Made by Jake</footer>";
+      "<div id='overlay'></div><div id='popup'><h2 id='currentShipHeader'></h2><div id='placeShipsBoard'></div></div><header><h1>BATTLESHIP</h1></header><main><div id='boards'><div id='p1Board'></div><div id='p2Board'></div></div></main><footer>Made by Jake</footer>";
   }
 
   function renderGameboard(playerBoard, board) {
-    const p1Board = document.querySelector(".p1Board");
-    const p2Board = document.querySelector(".p2Board");
+    const p1Board = document.querySelector("#p1Board");
+    const p2Board = document.querySelector("#p2Board");
+    const popupBoard = document.querySelector("#placeShipsBoard");
     let container;
     if (playerBoard === "p1") container = p1Board;
-    else container = p2Board;
+    else if (playerBoard === "p2") container = p2Board;
+    else if (playerBoard === "popupBoard") container = popupBoard;
     for (let i = 0; i < 10; i += 1) {
       const boardColumn = document.createElement("div");
       boardColumn.setAttribute("class", "boardColumn");
@@ -20,21 +22,27 @@ function Dom() {
       for (let j = 0; j < 10; j += 1) {
         const cell = document.createElement("div");
         cell.classList.add(
-          `${playerBoard}cell`,
+          `${playerBoard}Cell`,
           `${board.getBoard()[i][j][0]}`,
         );
-        cell.setAttribute("data-coordinate", `${[i]}${[j]}`);
+        cell.dataset.x = i;
+        cell.dataset.y = j;
         boardColumn.appendChild(cell);
       }
     }
   }
+  const p1Board = document.querySelector(".p1Board");
+  const p2Board = document.querySelector(".p2Board");
+  const popupBoard = document.querySelector("#placeShipsBoard");
 
   function clearGameboard(player) {
-    const p1Board = document.querySelector(".p1Board");
-    const p2Board = document.querySelector(".p2Board");
+    // const p1Board = document.querySelector(".p1Board");
+    // const p2Board = document.querySelector(".p2Board");
+    // const popupBoard = document.querySelector("#placeShipsBoard");
     let playerBoard;
     if (player === "p1") playerBoard = p1Board;
-    else playerBoard = p2Board;
+    else if (player === "p2") playerBoard = p2Board;
+    else if (player === "popupBoard") playerBoard = popupBoard;
     while (playerBoard.firstChild)
       playerBoard.removeChild(playerBoard.lastChild);
   }
@@ -43,8 +51,8 @@ function Dom() {
   }
 
   function shoot(cell, gameTurns, player) {
-    const x = cell.dataset.coordinate[0];
-    const y = cell.dataset.coordinate[1];
+    const { x } = cell.dataset;
+    const { y } = cell.dataset;
     player.gameboard.receiveAttack(x, y);
     gameTurns();
   }
@@ -57,13 +65,76 @@ function Dom() {
     });
   }
 
+  function currentShip(shipIndex) {
+    const shipArray = [
+      { shipName: "Carrier", size: 5 },
+      { shipName: "Battleship", size: 4 },
+      { shipName: "Destroyer", size: 3 },
+      { shipName: "Submarine", size: 3 },
+      { shipName: "Patrol Boat", size: 2 },
+    ];
+    return shipArray[shipIndex];
+  }
+
+  function hoverShipDisplay(cell, shipIndex, movement) {
+    const { size } = currentShip(shipIndex);
+    const x = Number(cell.dataset.x);
+    const y = Number(cell.dataset.y);
+    if (movement === "enter") {
+      for (let i = x - 1; i < x + size - 1; i += 1) {
+        if (i >= 9) return;
+        const targetCell = document.querySelector(
+          `[data-x='${i + 1}'][data-y='${y}']`,
+        );
+        targetCell.classList.add("hoverShip");
+      }
+    } else
+      for (let i = x - 1; i < x + size - 1; i += 1) {
+        if (i >= 9) return;
+        // search/add even listeners here
+        const targetCell = document.querySelector(
+          `[data-x='${i + 1}'][data-y='${y}']`,
+        );
+        targetCell.classList.remove("hoverShip");
+      } // cell.classList.remove("hoverShip");
+  }
+
+  function dismissPopup() {}
+
+  function placeShipsPopup(gameboard, shipIndex = 0) {
+    const currentShipHeader = document.querySelector("#currentShipHeader");
+    if (popupBoard) clearGameboard("popupBoard");
+    renderGameboard("popupBoard", gameboard);
+    currentShipHeader.textContent = `Place your ${
+      currentShip(shipIndex).shipName
+    }`;
+    const popupBoardCells = document.querySelectorAll(".popupBoardCell");
+
+    popupBoardCells.forEach((cell) =>
+      cell.addEventListener(
+        "mouseenter",
+        () => {
+          hoverShipDisplay(cell, shipIndex, "enter");
+        },
+        cell.addEventListener("mouseleave", () =>
+          hoverShipDisplay(cell, shipIndex, "leave"),
+        ),
+        cell.addEventListener("click"),
+      ),
+    );
+
+    // on click, call p2.gameboard.placeShip(length, x, y)
+    // if (shipIndex < (shipArray.length)){placeShipsPopup(gameboard, shipIndex + 1)}
+    // else dismissPopup()
+  }
+
   return {
     renderGameboard,
     clearGameboard,
     declareWinner,
     initPage,
     playerClicks,
-    // disableAttacks,
+    placeShipsPopup,
   };
 }
 
